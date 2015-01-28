8b9a4ae8372d46209c0c42e7f4850b2795bd44d0
jaly50 <jialic@andrew.cmu.edu>
2015-01-28 18:09:50 -0500

    create

--- src/employeeFormbeans/CreateFundForm.java
+++ src/employeeFormbeans/CreateFundForm.java
@@ -8,7 +8,7 @@ import org.mybeans.form.FormBean;
 public class CreateFundForm  extends FormBean  {
 	private String name;
 	private String symbol;
-	static String pattern= "^[a-zA-Z0-9._-]*$";
+	static String pattern= "^[a-zA-Z0-9]*$";
 	static String symbolPattern = "^[A-Z]*$";
 	public String getName() {
 		return name;
@@ -32,7 +32,7 @@ public class CreateFundForm  extends FormBean  {
 			errors.add("Please input a shorter fund name(with 30 characters)");
 		}
 		if (!name.matches(pattern)) {
-			errors.add("Only characters, numbers, and \"-\", \"_\", \".\" are allowed for fund name.");
+			errors.add("Only characters, numbers are allowed for fund name.");
 		}
 
 		if (symbol == null || symbol.length() == 0) {
